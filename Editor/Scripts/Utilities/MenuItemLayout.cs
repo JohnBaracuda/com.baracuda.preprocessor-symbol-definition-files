88e1f1776cb905a235b45697bffef5ccb09334cd
John Baracuda <58704151+JohnBaracuda@users.noreply.github.com>
2022-10-30 19:52:27 +0100

    Documentation Update
    • Added Documentation in multiple file formats to samples.

--- Editor/Scripts/Utilities/MenuItemLayout.cs
+++ Editor/Scripts/Utilities/MenuItemLayout.cs
@@ -4,7 +4,7 @@ namespace Baracuda.PreprocessorDefinitionFiles.Scripts.Utilities
 {
     internal static class MenuItemLayout
     {
-        [MenuItem("Tools/Preprocessor-Symbol-Definition-File", priority = 2360)]
+        [MenuItem("Tools/Preprocessor Symbol Definition Files", priority = 2360)]
         public static void SelectPreprocessorSymbolDefinitionSettings()
         {
             PreprocessorSymbolDefinitionSettings.Select();
